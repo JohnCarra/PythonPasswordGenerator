34154bfa7ae5c6f452648dfbe5dfc00206bc4f34
Johnathan Carranza <carranza978@gmail.com>
2023-03-16 21:40:41 -0700

    Styling and increased pass length
    added some styling and increased the password length to 16.

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Password Generator")
 
+# Define the font
+font = ("Arial", 14)
+
+# Create a frame to hold the label and button
+frame = tk.Frame(root)
+frame.pack(pady=20)
+
 # Create a label widget
-label = tk.Label(root, text="Generate a Password", font=("Arial", 14))
+label = tk.Label(
+    frame, text="Click the button to generate a password", font=font)
 label.pack(pady=10)
 
 # Create a function to generate a random password
@@ -15,7 +23,7 @@ label.pack(pady=10)
 
 def generate_password():
     # Define the password length
-    password_length = 12
+    password_length = 16
 
     # Define the characters to use for the password
     characters = string.ascii_letters + string.digits + string.punctuation
@@ -29,8 +37,18 @@ def generate_password():
 
 
 # Create a button to generate the password
-button = tk.Button(root, text="Generate", command=generate_password)
+button = tk.Button(frame, text="Generate Password", font=font, bg="#1E90FF", fg="white", activebackground="#007FFF",
+                   activeforeground="white", bd=0, padx=10, pady=5, relief="groove", command=generate_password)
 button.pack()
+
+# Set the window size and center it on the screen
+window_width = 300
+window_height = 200
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 # Start the GUI event loop
 root.mainloop()
